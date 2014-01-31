a915c91d5c88efbec2e24d5a4dd663745e49c7d5
Tim Kim <timk@adobe.com>
2014-01-30 16:14:47 -0800

    [#34] - made handshake work with client

--- res/middleware/autoreload.js
+++ res/middleware/autoreload.js
@@ -4,7 +4,20 @@
 //
 (function() {
 
-    function checkForReload() {
+    function postStatus(){
+        var xhr = new XMLHttpRequest;
+        xhr.open('post', 'http://' + document.location.host + '/autoreload', false);
+        xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && /^[2]/.test(this.status)) {
+                // do we need to read the response?
+                var response = JSON.parse(this.responseText);
+            }
+        }
+        xhr.send();
+    }
+
+    function checkForReload(){
         var xhr = new XMLHttpRequest;
         xhr.open('get', 'http://' + document.location.host + '/autoreload', true);
         xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
@@ -12,12 +25,14 @@
             if (this.readyState === 4 && /^[2]/.test(this.status)) {
                 var response = JSON.parse(this.responseText);
                 if (response.outdated) {
+                    postStatus();
                     window.location.reload();
                 }
             }
         }
         xhr.send();
     }
+
 
     setInterval(checkForReload, 1000 * 3);
 
