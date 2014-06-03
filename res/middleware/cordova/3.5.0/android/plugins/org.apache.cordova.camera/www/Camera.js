0c8c38d13d628f43142a07e124908ce86eb51542
Michael Brooks <michael@michaelbrooks.ca>
2014-06-03 16:29:24 -0700

    [#66] Add org.apache.cordova.camera plugin for Android iOS WP8 3.5.0

--- res/middleware/cordova/3.5.0/android/plugins/org.apache.cordova.camera/www/Camera.js
+++ res/middleware/cordova/3.5.0/android/plugins/org.apache.cordova.camera/www/Camera.js
@@ -1,4 +1,4 @@
-cordova.define("org.apache.cordova.camera.camera", function(require, exports, module) {/*
+cordova.define("org.apache.cordova.camera.camera", function(require, exports, module) { /*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -73,4 +73,5 @@ cameraExport.cleanup = function(successCallback, errorCallback) {
 };
 
 module.exports = cameraExport;
+
 });
