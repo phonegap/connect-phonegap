e8dff0927a3bf98ee3440a9bac4e0e6d1aeb4ce7
Jesse MacFadyen <purplecabbage@gmail.com>
2014-06-05 15:58:45 -0700

    [#57] Add console.log support

---
+++ res/middleware/consoler.js
@@ -0,0 +1,19 @@
+<script src="/socket.io/socket.io.js"></script>
+<script>
+    var socket = io('http://' + document.location.host);
+    var previousConsole = window.console || {};
+    window.console = {
+        log:function(msg){
+            previousConsole.log && previousConsole.log(msg);
+            socket.emit('console','log', msg);
+        },
+        warn:function(msg){
+           previousConsole.warn && previousConsole.warn(msg);
+            socket.emit('console','warn', msg);
+        },
+        error:function(msg){
+            previousConsole.error && previousConsole.error(msg);
+            socket.emit('console','error', msg);
+        }
+    }
+</script>
