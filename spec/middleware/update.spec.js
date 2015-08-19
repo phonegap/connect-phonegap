d046f646f7a95316a10e3e96ca5f41482f79734c
timkim <timk@adobe.com>
2015-08-19 15:39:12 -0700

    [#122] - testing update functionality

--- spec/middleware/update.spec.js
+++ spec/middleware/update.spec.js
@@ -4,7 +4,6 @@
 
 var events = require('events'),
     fs = require('fs'),
-    archiver = require('archiver'),
     gaze = require('gaze'),
     path = require('path'),
     phonegap = require('../../lib'),
@@ -12,7 +11,11 @@ var events = require('events'),
     shell = require('shelljs'),
     zip = require('../../lib/middleware/zip'),
     url = '/__api__/update',
-    watchSpy;
+    agent,
+    watchSpy,
+    update,
+    archive,
+    testPath = path.resolve(__dirname, '../fixture/app-without-cordova');
 
 /*!
  * Specification: Middleware for /update route
@@ -20,85 +23,34 @@ var events = require('events'),
 
 describe('update middleware', function() {
     beforeEach(function() {
-        spyOn(gaze, 'Gaze').andReturn({ on: function() {} });
-        spyOn(process, 'cwd').andReturn(path.resolve(__dirname, '../fixture/app-without-cordova'));
+        watchSpy = new events.EventEmitter();
+        spyOn(gaze, 'Gaze').andReturn(watchSpy);
+        spyOn(process, 'cwd').andReturn(testPath);
     });
 
     describe('GET /__api__/update', function() {
-        it('should generate a zip', function(done) {
-            createSpy = jasmine.createSpy('create');
-            spyOn(archiver, 'create').andCallFake(createSpy);
 
-            request(phonegap())
-            .get('/__api__/update')
-            .end(function(e, res) {
-                expect(createSpy).toHaveBeenCalled();
-                done();
-            });
-        });
-    
         describe('successfully generated zip', function() {
-            it('should have a 200 response code', function(done) {
-                request(phonegap())
-                .get('/__api__/update')
-                .end(function(e, res) {
-                    expect(res.statusCode).toEqual(200);
-                    done();
-                });
-            });
-
-            it('should have application/zip Content-Type', function(done) {
-                request(phonegap())
-                .get('/__api__/update')
-                .end(function(e, res) {
-                    expect(res.headers['content-type']).toEqual('application/zip');
-                    done();
-                });
-            });
-
-            it('should respond with the zip content', function(done) {
-                request(phonegap())
-                .get('/__api__/update')
-                // custom application/zip parser for supertest
-                .parse(function(res, callback) {
-                    res.setEncoding('binary');
-                    res.data = '';
-                    res.on('data', function (chunk) {
-                        res.data += chunk;
-                    });
-                    res.on('end', function () {
-                        callback(null, new Buffer(res.data, 'binary'));
-                    });
-                })
-                .end(function(e, res) {
-                    expect(Buffer.isBuffer(res.body)).toBe(true);
-                    expect(res.body.length).toBeGreaterThan(0);
-                    done();
-                });
+            beforeEach(function() {
+                update = require('../../lib/middleware/update');
+                archive = update.archiver();
+                agent = request.agent(phonegap());
             });
 
             it('should respond with only the updated content', function(done) {
-                done();
-            });
-        });
+                spyOn(archive, 'append').andCallThrough();
 
-        describe('failed to generate zip', function() {
-            beforeEach(function() {
-                spyOn(archiver, 'create').andCallFake(function() {
-                    throw new Error();
-                });
-            });
+                agent.get('/__api__/autoreload').end(function(e, res) {
+                    watchSpy.emit('all', 'eventType', path.join(process.cwd(),'www/index.html'));
 
-            it('should have a 500 response code', function(done) {
-                request(phonegap())
-                .get('/__api__/update')
-                .end(function(e, res) {
-                    expect(res.statusCode).toEqual(500);
-                    done();
+                    agent.get('/__api__/update').end(function(e, res) {
+                        expect(archive.append).toHaveBeenCalled();
+                        expect(archive.append).toHaveBeenCalledWith(jasmine.any(Object), jasmine.objectContaining({ name: 'www/index.html' }));
+                        done();
+                    });
                 });
             });
         });
-
     });
 
 });
