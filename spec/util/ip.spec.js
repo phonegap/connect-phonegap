08c15fea3166cbf30491f1491ddc9dee9dfbe66b
Michael Brooks <michael@michaelbrooks.ca>
2015-11-17 14:29:23 -0800

    [#77] Output all available IP Addresses for multi-NIC systems.

--- spec/util/ip.spec.js
+++ spec/util/ip.spec.js
@@ -4,123 +4,77 @@
 
 var ip = require('../../lib/util/ip'),
     ipAddress = require('ip'),
-    request = require('request');
+    os = require('os');
 
 /*!
  * Specification.
  */
 
 describe('ip', function() {
-    beforeEach(function() {
-        spyOn(request, 'get');
-    });
-
-    describe('.address(callback)', function() {
+    describe('.address(callback, address, addresses)', function() {
         it('should be a function', function () {
             expect(ip.address).toEqual(jasmine.any(Function));
         });
 
-        describe('when external host is reachable', function() {
+        describe('when one public IPv4 address is present', function() {
             beforeEach(function() {
-                request.get.andCallFake(function(options, callback) {
-                    var res = {
-                        req: {
-                            connection: {
-                                localAddress: '10.0.1.4'
-                            },
-                            socket: {
-                                localAddress: undefined
-                            }
-                        }
-                    };
-                    callback(null, res, 'data');
+                spyOn(os, 'networkInterfaces').andReturn({
+                    en0: [{
+                        family: 'IPv4',
+                        internal: false,
+                        address: '10.0.1.4'
+                    }]
                 });
             });
 
             it('should not return an error', function(done) {
-                ip.address(function(e, address) {
-                    expect(e).toBeNull();
-                    done();
-                });
-            });
-
-            it('should return an ip address from request', function(done) {
-                ip.address(function(e, address) {
-                    expect(address).toEqual('10.0.1.4');
-                    done();
-                });
-            });
-
-            describe('when connect.localAddress is undefined', function() {
-                beforeEach(function() {
-                    request.get.andCallFake(function(options, callback) {
-                        var res = {
-                            req: {
-                                connection: {
-                                    localAddress: undefined
-                                },
-                                socket: {
-                                    localAddress: '10.0.1.5'
-                                }
-                            }
-                        };
-                        callback(null, res, 'data');
-                    });
-                });
-
-                it('should return the socket localAddress', function(done) {
-                    ip.address(function(e, address) {
-                        expect(address).toEqual('10.0.1.5');
-                        done();
-                    });
-                });
-            });
-
-            describe('when socket.localAddress is undefined', function() {
-                beforeEach(function() {
-                    spyOn(ipAddress, 'address').andCallThrough();
-                    request.get.andCallFake(function(options, callback) {
-                        var res = {
-                            req: {
-                                connection: {
-                                    localAddress: undefined
-                                },
-                                socket: {
-                                    localAddress: undefined
-                                }
-                            }
-                        };
-                        callback(null, res, 'data');
-                    });
-                });
-
-                it('should lookup the network adapter address', function(done) {
-                    ip.address(function(e, address) {
-                        expect(ipAddress.address).toHaveBeenCalled();
-                        expect(address).toEqual(ipAddress.address());
-                        done();
-                    });
-                });
-            });
-        });
-
-        describe('when external host is not reachable', function() {
-            beforeEach(function() {
-                request.get.andCallFake(function(options, callback) {
-                    callback(new Error('getaddrinfo ENOTFOUND'));
-                });
-            });
-
-            it('should not return an error', function(done) {
-                ip.address(function(e, address) {
+                ip.address(function(e, address, addresses) {
                     expect(e).toBeNull();
                     done();
                 });
             });
 
             it('should return ip address from adapter', function(done) {
-                ip.address(function(e, address) {
-                    expect(address).toEqual(require('ip').address());
+                ip.address(function(e, address, addresses) {
+                    expect(address).toEqual('10.0.1.4');
+                    expect(addresses[0]).toEqual('10.0.1.4');
+                    expect(addresses.length).toEqual(1);
+                    done();
+                });
+            });
+        });
+
+        describe('when two or more public IPv4 addresses are present', function() {
+            beforeEach(function() {
+                spyOn(os, 'networkInterfaces').andCallFake(function() {
+                    return {
+                        en0: [{
+                            family: 'IPv4',
+                            internal: false,
+                            address: '10.0.1.4'
+                        }],
+                        en1: [{
+                            family: 'IPv4',
+                            internal: false,
+                            address: '10.0.1.5'
+                        }]
+                    };
+                });
+            });
+
+            it('should not return an error', function(done) {
+                ip.address(function(e, address, addresses) {
+                    expect(e).toBeNull();
+                    done();
+                });
+            });
+
+            it('should return ip addresses from adapter', function(done) {
+                ip.address(function(e, address, addresses) {
+                    expect(address).toEqual('10.0.1.4');
+                    expect(addresses[0]).toEqual('10.0.1.4');
+                    expect(addresses[1]).toEqual('10.0.1.5');
+                    expect(addresses.length).toEqual(2);
                     done();
                 });
             });
