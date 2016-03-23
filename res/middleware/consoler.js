6df70de5919252ff48424a9a3d7a197915d32b4b
Anis Kadri <anis@apache.org>
2016-03-22 18:27:18 -0700

    Attempting to replace socket.io with ws

--- res/middleware/consoler.js
+++ res/middleware/consoler.js
@@ -1,27 +1,27 @@
-<script type="text/javascript" src="http://127.0.0.1:3000/socket.io/socket.io.js"></script>
 <script type="text/javascript">
 (function(window) {
-    var socket = io('http://127.0.0.1:3000');
-    var previousConsole = window.console || {};
-    window.console = {
-        log:function(msg){
-            previousConsole.log && previousConsole.log(msg);
-            socket.emit('console','log', msg);
-        },
-        warn:function(msg){
-            previousConsole.warn && previousConsole.warn(msg);
-            socket.emit('console','warn', msg);
-        },
-        error:function(msg){
-            previousConsole.error && previousConsole.error(msg);
-            socket.emit('console','error', msg);
-        },
-        assert:function(assertion, msg){
-            previousConsole.assert && previousConsole.assert(assertion, msg);
-            if(assertion){
-                socket.emit('console','assert', msg);
-            }
-        }
+  var socket = new WebSocket('ws://127.0.0.1:3000');
+  var previousConsole = window.console || {};
+  window.console = {
+    log:function(msg){
+      previousConsole.log && previousConsole.log(msg);
+      socket.send('log%!@$#'+msg);
+    },
+    warn:function(msg){
+      previousConsole.warn && previousConsole.warn(msg);
+      socket.send('warn%!@$#'+msg);
+    },
+    error:function(msg){
+      previousConsole.error && previousConsole.error(msg);
+      socket.send('error%!@$#'+msg);
+    },
+    assert:function(assertion, msg){
+      previousConsole.assert && previousConsole.assert(assertion, msg);
+      if(assertion){
+        socket.send('assert%!@$#'+msg);
+      }
     }
+  }
 })(window);
+
 </script>
