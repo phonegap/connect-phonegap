c10eb4d53c5cda3444a056f768ded81607ec187e
Anis Kadri <anis@apache.org>
2016-04-06 18:32:37 -0700

    overriding cordova.exec

--- res/middleware/devices.js
+++ res/middleware/devices.js
@@ -1,15 +1,26 @@
 <script type="text/javascript">
 (function() {
+    var phonegap = phonegap || {};
+
+    phonegap.getSelectedDevice = function() {
+      var devicesEm = document.getElementById("phonegap_devices");
+      if(devicesEm) {
+        return devicesEm.options[devicesEm.selectedIndex].value;
+      };
+      return null;
+    };
+
 
     var url = 'http://' + document.location.host + '/__api__/devices';
 
     function updateDeviceList(devices) {
         if(devices.length > 0) {
+          phonegap.devices = devices;
           var devicesEm = document.createElement("select");
           devicesEm.style.position = "absolute";
           devicesEm.style.top = "0px";
           devicesEm.style.right = "0px";
-          devicesEm.id = "devices";
+          devicesEm.id = "phonegap_devices";
           for(var i = 0 , j = devices.length ; i < j ; i++) {
             var deviceEm = document.createElement("option");
             deviceEm.text = devices[i].device.platform + " " + devices[i].device.version;
@@ -17,7 +28,7 @@
             deviceEm.style["background-color"] = "green";
             devicesEm.add(deviceEm);
           }
-          var exDevicesEm = document.getElementById("devices");
+          var exDevicesEm = document.getElementById("phonegap_devices");
           if(exDevicesEm) {
             var exDevicesParentEm = exDevicesEm.parentNode;
             exDevicesParentEm.replaceChild(devicesEm, exDevicesEm);
@@ -41,6 +52,60 @@
         }
         xhr.send();
     }
+    
+    (function overrideBrowserExec() {
+
+      cordova.define.remove("cordova/exec");
+    
+      cordova.define("cordova/exec", function(require, exports, module) {
+        var cordova = require('cordova');
+        var execProxy = require('cordova/exec/proxy');
+
+        module.exports = function(success, fail, service, action, args) {
+
+            var proxy = execProxy.get(service, action);
+
+            if (proxy) {
+                var callbackId = service + cordova.callbackId++;
+
+                if (typeof success == "function" || typeof fail == "function") {
+                    cordova.callbacks[callbackId] = {success:success, fail:fail};
+                }
+
+                try {
+                    if(phonegap.getSelectedDevice() !== null) {
+                      var socket = new WebSocket("ws://"+phonegap.getSelectedDevice());
+                      var data = {
+                        service: service,
+                        action: action,
+                        args: []
+                      };
+                      socket.onmessage = function(event) {
+                        var res = JSON.parse(event.data);
+                        console.log(res);
+                        success(event.data);
+                      };
+                      socket.onopen = function(event) {
+                        socket.send(JSON.stringify(data));
+                      };
+                    } else {
+                      console.log("no FUCKING socket");
+                      proxy(success, fail, args);
+                    }
+                }
+                catch(e) {
+                    // TODO throw maybe?
+                    var msg = "Exception calling :: " + service + " :: " + action  + " ::exception=" + e;
+                    console.log(msg);
+                }
+            }
+            else {
+                fail && fail("Missing Command Error");
+            }
+        };
+
+      });
+    })(window);
 
     setInterval(checkForDevices, 1000 * 10);
 
