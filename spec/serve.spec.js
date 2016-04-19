0e276ee76c4a6cbdb4138073fb80fab076ffa158
Anis Kadri <anis@apache.org>
2016-04-18 17:19:57 -0700

    spying setInterval in tests

--- spec/serve.spec.js
+++ spec/serve.spec.js
@@ -28,6 +28,7 @@ describe('phonegap.serve(options, [callback])', function() {
             callback(null, res, 'data');
         });
         spyOn(socketServer, 'attachConsole');
+        spyOn(global, 'setInterval');
     });
 
     it('should not require options', function() {
