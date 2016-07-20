a8826ac3b87763909249486fed9cf75f992f1cfc
Suraj Pindoria <suraj.pindoria@yahoo.com>
2016-07-20 11:37:08 -0700

    [#166] - Combine console arguments with spaces over tabs

--- res/middleware/consoler.js
+++ res/middleware/consoler.js
@@ -6,26 +6,26 @@
             if(previousConsole.log) {
                 previousConsole.log.apply(previousConsole, arguments);
             }
-            socket.emit('console','log', Array.prototype.slice.call(arguments).join('\t'));
+            socket.emit('console','log', Array.prototype.slice.call(arguments).join(' '));
         },
         warn:function(){
             if(previousConsole.warn) {
                 previousConsole.warn.apply(previousConsole, arguments);
             }
-            socket.emit('console','warn', Array.prototype.slice.call(arguments).join('\t'));
+            socket.emit('console','warn', Array.prototype.slice.call(arguments).join(' '));
         },
         error:function(){
             if(previousConsole.error) {
                 previousConsole.error.apply(previousConsole, arguments);
             }
-            socket.emit('console','error', Array.prototype.slice.call(arguments).join('\t'));
+            socket.emit('console','error', Array.prototype.slice.call(arguments).join(' '));
         },
         assert:function(assertion) {
             if(previousConsole.assert) {
                 previousConsole.assert.apply(previousConsole, arguments);
             }
             if(assertion){
-                socket.emit('console','assert', Array.prototype.slice.call(arguments, 1).join('\t'));
+                socket.emit('console','assert', Array.prototype.slice.call(arguments, 1).join(' '));
             }
         }
     };
