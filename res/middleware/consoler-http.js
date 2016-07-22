d641eb232215578926b2c4cefd04ae3693f181c6
Joe Furfaro <joe.furfaro@thecontrolgroup.com>
2016-07-22 16:57:44 -0700

    Changes to preserve extra browser console methods

--- res/middleware/consoler-http.js
+++ res/middleware/consoler-http.js
@@ -1,32 +1,42 @@
 (function(window) {
     var socket = io('http://' + document.location.host);
-    var previousConsole = window.console || {};
-    window.console = {
-        log:function(){
-            if(previousConsole.log) {
-                previousConsole.log.apply(previousConsole, arguments);
-            }
-            socket.emit('console','log', Array.prototype.slice.call(arguments).join(' '));
-        },
-        warn:function(){
-            if(previousConsole.warn) {
-                previousConsole.warn.apply(previousConsole, arguments);
-            }
-            socket.emit('console','warn', Array.prototype.slice.call(arguments).join(' '));
-        },
-        error:function(){
-            if(previousConsole.error) {
-                previousConsole.error.apply(previousConsole, arguments);
-            }
-            socket.emit('console','error', Array.prototype.slice.call(arguments).join(' '));
-        },
-        assert:function(assertion) {
-            if(previousConsole.assert) {
-                previousConsole.assert.apply(previousConsole, arguments);
-            }
-            if(assertion){
-                socket.emit('console','assert', Array.prototype.slice.call(arguments, 1).join(' '));
-            }
+    
+    // Copy the functions to avoid stack overflow
+    var previousConsole = {
+        log: window.console.log.bind(this),
+        warn: window.console.warn.bind(this),
+        error: window.console.error.bind(this),
+        assert: window.console.assert.bind(this)
+    };
+    
+    // Overwrite these individually to preserve other log properties
+    window.console.log = function(){
+        if(previousConsole.log) {
+            previousConsole.log.apply(previousConsole, arguments);
         }
-    }
+        socket.emit('console','log', Array.prototype.slice.call(arguments).join(' '));
+    };
+    
+    window.console.warn = function(){
+        if(previousConsole.warn) {
+            previousConsole.warn.apply(previousConsole, arguments);
+        }
+        socket.emit('console','warn', Array.prototype.slice.call(arguments).join(' '));
+    };
+    
+    window.console.error = function(){
+        if(previousConsole.error) {
+            previousConsole.error.apply(previousConsole, arguments);
+        }
+        socket.emit('console','error', Array.prototype.slice.call(arguments).join(' '));
+    };
+    
+    window.console.assert = function(assertion) {
+        if(previousConsole.assert) {
+            previousConsole.assert.apply(previousConsole, arguments);
+        }
+        if(assertion){
+            socket.emit('console','assert', Array.prototype.slice.call(arguments, 1).join(' '));
+        }
+    };
 })(window);
