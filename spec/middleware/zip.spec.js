fa842cb97189480ea118ce6ea9d67c9559643e12
filmaj <maj.fil@gmail.com>
2017-04-22 14:57:20 -0700

    Skip symlink test on windows, as windows does not support symlinks.

--- spec/middleware/zip.spec.js
+++ spec/middleware/zip.spec.js
@@ -48,6 +48,7 @@ describe('zip middleware', function() {
         });
 
         it('should zip files for app with symlinks', function(done) {
+            if (process.platform == 'win32') done(); // skip this test on Windows as symlinks are not supported there!
             setApp('app-with-symlinks');
 
             request(phonegap())
