1c943eca232d0dadba72bd03ad1febe84a6ec931
filmaj <maj.fil@gmail.com>
2017-04-24 13:12:50 -0700

    #197: adding specs for redirect logic in proxy middleware.

--- spec/middleware/proxy.spec.js
+++ spec/middleware/proxy.spec.js
@@ -7,8 +7,10 @@ var chdir = require('chdir'),
     phonegap = require('../../lib'),
     request = require('supertest'),
     rewire = require('rewire'),
+    http = require('http'),
     middleware = rewire('../../lib/middleware/proxy'),
-    proxy, revert, web_spy, emit_spy, req;
+    proxy, revert, web_spy, emit_spy, once_spy, req,
+    test_server;
 
 /*!
  * Specification: Proxy middleware.
@@ -36,8 +38,9 @@ describe('proxy middleware', function() {
         beforeEach(function() {
             web_spy = jasmine.createSpy('proxy.web spy');
             emit_spy = jasmine.createSpy('emitter spy');
+            once_spy = jasmine.createSpy('proxy.once spy');
             revert = middleware.__set__('proxy', {
-                once:function() {},
+                once: once_spy,
                 web: web_spy
             });
             req = {
@@ -90,6 +93,53 @@ describe('proxy middleware', function() {
             expect(emit_spy).toHaveBeenCalledWith('error', 'Proxy error for url: http://filmaj/', err_msg);
             expect(res.writeHead).toHaveBeenCalledWith(500);
             expect(res.end).toHaveBeenCalledWith(err_msg);
+        });
+    });
+    describe('integration test with a separate server', function() {
+        beforeEach(function() {
+            spyOn(gaze, 'Gaze').and.returnValue({ on: function() {} });
+        });
+        afterEach(function() {
+            test_server.close();
+        });
+        it('should handle redirects to different paths on the same server appropriately', function(done) {
+            test_server = http.createServer(function (req, res) {
+                if (req.url.match(/redirect/)) {
+                    res.writeHead(302, {
+                        'Location': '/gohereplz'
+                    });
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.write('you have arrived');
+                }
+                res.end();
+            }).listen(9000);
+            chdir('spec/fixture/app-with-cordova', function() {
+                request(phonegap())
+                .get('/proxy/' + encodeURIComponent("http://localhost:9000/redirect"))
+                .end(function(e, res) {
+                    expect(res.statusCode).toEqual(302);
+                    expect(res.headers.location).toContain('/proxy/' + encodeURIComponent('http://localhost:9000/gohereplz'));
+                    done();
+                });
+            });
+        });
+        it('should handle redirects to different domains appropriately', function(done) {
+            test_server = http.createServer(function (req, res) {
+                res.writeHead(302, {
+                    'Location': 'http://phonegap.com/robots.txt'
+                });
+                res.end();
+            }).listen(9000);
+            chdir('spec/fixture/app-with-cordova', function() {
+                request(phonegap())
+                .get('/proxy/' + encodeURIComponent("http://localhost:9000/redirect"))
+                .end(function(e, res) {
+                    expect(res.statusCode).toEqual(302);
+                    expect(res.headers.location).toContain('/proxy/' + encodeURIComponent('http://phonegap.com/robots.txt'));
+                    done();
+                });
+            });
         });
     });
 });
