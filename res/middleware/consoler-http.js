811b2a2b5f6d684f183815c0140b28870cdfe9cd
Georgii Korshunov <gkorshunov@gog.com>
2017-07-15 02:31:56 +0200

    Using browser current protocol

--- res/middleware/consoler-http.js
+++ res/middleware/consoler-http.js
@@ -1,5 +1,5 @@
 (function(window) {
-    var socket = io('http://' + document.location.host);
+    var socket = io(document.location.protocol + '//' + document.location.host);
 
     // Copy the functions to avoid stack overflow
     var previousConsole = Object.assign({}, window.console);
